e8d2f3af82af6064d07571b3988510c781bf12f1
Homesh Sahu <83745527+homesh-sahu@users.noreply.github.com>
2025-01-19 19:29:20 +0530

    Update Sidebar.jsx

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -22,7 +22,7 @@ const Sidebar = () => {
   if (isUsersLoading) return <SidebarSkeleton />;
 
   return (
-    <aside className="h-full w-28 lg:w-72 border-r border-base-300 flex flex-col transition-all duration-200">
+    <aside className="h-full w-[6.5rem] sm:w-28 lg:w-72 border-r border-base-300 flex flex-col transition-all duration-200">
       <div className="border-b border-base-300 w-full p-5">
         <div className="flex items-center gap-2">
           <Users className="size-6" />
@@ -58,7 +58,7 @@ const Sidebar = () => {
                 <img
                   src={user.profilePic || "/demoProfile.png"}
                   alt={user.name}
-                  className="size-12 object-cover rounded-full"
+                  className="size-10 sm:size-12 object-cover rounded-full"
                 />
                 {onlineUsers.includes(user._id) && (
                   <span className="absolute bottom-0 right-0 size-3 bg-green-500 rounded-full ring-2 ring-zinc-900" />
